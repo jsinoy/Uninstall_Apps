f472f2239fe62f2a8503be5ddc9f2248e0d272ee
jsinoy <109936671+jsinoy@users.noreply.github.com>
2023-06-09 13:31:35 +0800

    Update UNINSTALL_UTORRENT.bat

--- UNINSTALL_UTORRENT.bat
+++ UNINSTALL_UTORRENT.bat
@@ -1,7 +1,7 @@
 powershell -c "Stop-Process -Name utorrent -force"
 %userprofile%\appdata\Roaming\utorrent\uTorrent.exe /uninstall /s
 powershell -c (Remove-Item -Path "C:\Users\*\AppData\Roaming\utorrent" -Force -Recurse)
-reg delete HKEY_CURRENT_USER\software\BitTorrent /f
+reg delete HKEY_CURRENT_USER\software\BitTorrentPersist /f
 reg delete HKEY_CURRENT_USER\software\BitTorrent\uTorrent /f
 reg delete HKEY_CLASSES_ROOT\.torrent /f
 reg delete HKEY_CLASSES_ROOT\Applications\uTorrent.exe /f
